31524a214c48e122e1d4bc352980381b05aed694
Azeez Adebayo <debaycisse@gmail.com>
2024-08-31 23:00:31 +0100

    implement task 6
    implements a function that validates a given password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,16 @@ def hash_password(password: str) -> bytes:
         a salted, hashed version of the given password.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Compares a given hashed password and a plain password for validation
+
+    Args:
+        hashed_password - an hashed password to be compared with a plain one
+        password - a plain string password to be compared with the hashed one
+
+    Returns:
+        True if the plain matches the hashed password, otherwise, False
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
