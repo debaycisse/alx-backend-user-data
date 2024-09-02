1607b141fa19a5125b98bfd128297b15c660457b
Azeez Adebayo <debaycisse@gmail.com>
2024-09-02 15:27:39 +0100

    implement task 1
    1. Error handler: Unauthorized
    handles unauthorized errror and tests it as well

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def test_unauthorized() -> None:
+    """GET /api/v1/unauthorized
+    Aborts connection
+    """
+    abort(401)
