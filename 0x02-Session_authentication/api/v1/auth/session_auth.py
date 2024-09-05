ad57dfba3eadecd4c205b5ef0c302b0c7d8b0c71
Azeez Adebayo <debaycisse@gmail.com>
2024-09-05 15:56:34 +0100

    implement task 6
    6. Use Session ID for identifying a User
    implement a method for session authentication which fetches cookie and use it to locate a curent user and returns it.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """This modules houses the definition for the Session Authentication"""
 
 from .auth import Auth
+from models.user import User
 import uuid
 
 
@@ -40,3 +41,24 @@ class SessionAuth(Auth):
         if session_id is None or (not isinstance(session_id, str)):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Retrieves a user's instance based
+        on the cookie, passed in a request
+
+        Args:
+            request - the request object
+            that contains the cookie among other data
+
+        Returns:
+            an instance of a user class is returned, if found, otherwise None
+        """
+        if request is None:
+            return None
+        session_id = self.session_cookie(request=request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id=session_id)
+        if user_id:
+            return User.get(user_id)
+        return None
