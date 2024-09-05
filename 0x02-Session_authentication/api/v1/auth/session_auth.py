4f553605138aa766a937bcd5c2b41e106c1cdaf3
Azeez Adebayo <debaycisse@gmail.com>
2024-09-05 13:59:06 +0100

    implement task 3
    3. User ID for Session ID
    implements a method for the session authentication, which retrieves user's id based on its session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """creates a Session ID for a given user_id
-        
+
         Args:
             user_id - id of a user instance for
             whom a session ID is to be created
@@ -25,3 +25,18 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves a user ID, based on a provided session ID
+
+        Args:
+            session_id - the session ID whose
+            associated user ID is to be retrieved
+
+        Returns:
+            user ID that is associated with
+            the given session ID, otherwise None
+        """
+        if session_id is None or (not isinstance(session_id, str)):
+            return None
+        return self.user_id_by_session_id.get(session_id)
