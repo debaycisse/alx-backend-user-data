3411e76b2ab9fc2fb387b58267c0ace17bcb2dce
Azeez Adebayo <debaycisse@gmail.com>
2024-09-10 14:55:08 +0100

    implement task 11
    11. Log in
    implement a route that allows users to login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,9 @@
 from flask import (
     Flask,
     jsonify,
-    request
+    request,
+    abort,
+    Response
 )
 from auth import Auth
 
@@ -34,6 +36,21 @@ def users():
         )
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Logs a typical user in to the system"""
+    email: str = request.form.get("email")
+    password: str = request.form.get("password")
+    if not (AUTH.valid_login(email, password)):
+        abort(401)
+    session_id: str = AUTH.create_session(email)
+    res_obj: Response = jsonify(
+        {"email": "{}".format(email), "message": "logged in"}
+    )
+    res_obj.set_cookie("session_id", session_id)
+    return res_obj
 
 
 if __name__ == "__main__":
