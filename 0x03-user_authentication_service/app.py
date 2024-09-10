aaefabacb2db9e851346ae678dd679384cb1c29a
Azeez Adebayo <debaycisse@gmail.com>
2024-09-10 12:08:52 +0100

    implement task 7
    7. Register user
    implement an end-point that adds a new user to the database

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python3
 """This module houses the implementation of a flask application"""
-from flask import Flask, jsonify
+from flask import (
+    Flask,
+    jsonify,
+    request
+)
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def entry_point():
     """Serves the entry point of the program"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """Creates a new user object and stores it in a database"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email is None or password is None:
+        return jsonify(
+            {"message": "Neither email or password can be empty"}
+        ), 400
+    try:
+        AUTH.register_user(email, password)
+        return jsonify(
+            {"email": "{}".format(email), "message": "user created"}
+        )
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
