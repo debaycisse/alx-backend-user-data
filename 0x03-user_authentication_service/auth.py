1d9c9d0482a3cefbadad1409217fdd98f4f42f56
Azeez Adebayo <debaycisse@gmail.com>
2024-09-10 12:52:21 +0100

    implement task 8
    8. Credentials validation
    implement a method for the Authentication that
    checks if a provided user's credential is valid or not

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -16,7 +17,7 @@ def _hash_password(password: str) -> bytes:
     Returns:
         a hashed version of the password is hashed
     """
-    hash_pwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    hash_pwd: bytes = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hash_pwd
 
 
@@ -38,10 +39,30 @@ database.
             the uswr object of the new user, if no error, otherwise an error
         """
         try:
-            user = self._db.find_user_by(email=email)
+            user: User = self._db.find_user_by(email=email)
             if not (user is None):
                 raise ValueError("User {0} already exists".format(email))
         except NoResultFound:
-            pwd = _hash_password(password)
+            pwd: bytes = _hash_password(password)
             user = self._db.add_user(email, pwd)
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates a given user's credentials and indicates if it is valid
+
+        Args:
+            email - an email of the user whose credentials are to be validated
+            password - the password of the user
+
+        Returns:
+            True if the credentials are correct, otherwise False
+        """
+        try:
+            user: User = self._db.find_user_by(email=email)
+            if user is None:
+                return False
+        except NoResultFound:
+            return False
+        if bcrypt.checkpw(password.encode(), user.hashed_password):
+            return True
+        return False
