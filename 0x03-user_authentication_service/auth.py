518d32d2318a1544aa8463879c39098ae53d8aa9
Azeez Adebayo <debaycisse@gmail.com>
2024-09-10 08:45:44 +0000

    partially implement task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """This module houses the implementation
 of the authentication aspect of the program"""
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -15,3 +17,28 @@ def _hash_password(password: str) -> bytes:
     """
     hash_pwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hash_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication 
+database.
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def register_uswr(self, email: str, password: str) -> User:
+        """Registers a new user and stores the user's data in the database
+
+        Args:
+            email - email of the new user
+            password - password of the new user
+
+        Returns:
+            the uswr object of the new user, if no error, otherwise an error
+        """
+        user = self._db.find_user_by(email=email)
+        if not (user is None):
+            raise ValueError("User {0} already exists.".format(email))
+        pwd = _hash_password(password)
+        user = self._db.add_user(email, pwd)
+        return user
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,9 +6,9 @@ from sqlalchemy import (
     Integer,
     String
 )
-from typing import Any
+from sqlalchemy.orm import DeclarativeMeta
 
-Base: Any = declarative_base()
+Base: DeclarativeMeta = declarative_base()
 
 
 class User(Base):
